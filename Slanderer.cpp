555d1ff8ce3c88fde68d440478bfe13dc4b4dbf3
PouPeuu <qwertydude277@gmail.com>
2023-08-01 14:46:04 +0300

    Added some stuff

--- Slanderer.cpp
+++ Slanderer.cpp
@@ -20,10 +20,10 @@ int main(int argc, char* argv[]){
 	string subjects[] = {"You", "I", "They", "We"};
 	string adverbs[] = {"like", "love", "strive", "want", "need", "desire"};
 	string es_adverbs[] = {"wish"};
-	string verbs[] = {"eat", "lick", "sniff", "snort", "squish", "crush", "piss on", "fart on", "poo n kak on", "pop a fart on"};
+	string verbs[] = {"eat", "lick", "sniff", "snort", "squish", "crush", "piss on", "fart on", "poo n kak on", "pop a fart on", "contemplate", "examine", "research", "imprison", "retrieve","perform unlicenced experiments on", "sell", "cook", "hunt down", "return", "obliterate"};
 	string first_adjective[] = {"copious amounts of", "unreasonably", "horribly", "terribly", "repellingly", "reasonably", "slightly", "very", "extremely", "all of the", "stupidly"};
 	string secondary_adjective[] = {"greasy", "disgusting", "smelly", "sweaty", "good-smelling", "tasty", "nice", "shitty", "nerdy", "rancid", "yucky", "decent", "good"};
-	string objects[] = {"shit", "feet", "teeth", "eyeballs", "piss", "toilets", "gas station toilets", "discord servers", "reddit moderators", "Cyblu Bots", "nerds", "people", "lines of cocaine", "create aeronautics release date predictions"};
+	string objects[] = {"shit", "feet", "teeth", "eyeballs", "piss", "toilets", "gas station toilets", "discord servers", "reddit moderators", "Cyblu Bots", "nerds", "people", "lines of cocaine", "crystal meth", "create aeronautics release date predictions", "people that say that create aeronautics wont be released", "people that say that create aeronautics developers are on summer break", "people who simply speak the truth", "ketxhup that he stole"};
 
 	int amount = stoi(argv[1]);
 	string target = "";
